74d7eacbb79f387471a859c5fda182ce0a6827f8
kaklik <kaklik@users.noreply.github.com>
2026-02-13 22:36:49 +0000

    Fix firmware build workflow, extended build

--- SW/arduino/src/TFUNIPAYLOAD_MINIMAL/githash.h
+++ SW/arduino/src/TFUNIPAYLOAD_MINIMAL/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "82ea3b61cf32cbdf8d407673a4e7e27cde9f0c65,User";
+String githash = "2b745caccb1765d0b1792959b71a572b00862f21,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
