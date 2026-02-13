0b820213e557434eb2825a6a931f790cbb0e1484
roman-dvorak <roman-dvorak@users.noreply.github.com>
2026-02-13 20:34:59 +0000

    Fix firmware workflow paths after compile to avoid wrong working directory, extended build, extended build, extended build, extended build, extended build, extended build, extended build

--- SW/arduino/src/TFUNIPAYLOAD/githash.h
+++ SW/arduino/src/TFUNIPAYLOAD/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "d8e8f1ee7141f59e3de8037f954fac6bf37936c9,User";
+String githash = "112cdc4fab5e1d6f3d197b7d6d63df38b1e756ce,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
