6fadc91ae95a0140e736e40079b8e3f6547bf3e7
roman-dvorak <roman-dvorak@users.noreply.github.com>
2026-02-13 22:43:32 +0000

    Update build_fw.yml, extended build, extended build

--- SW/arduino/src/TFUNIPAYLOAD/githash.h
+++ SW/arduino/src/TFUNIPAYLOAD/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "18c0e9ffc0f1f8b6992a4eeb77d82ab872d522d6,User";
+String githash = "7496b9e5c39a4f5228417475df0bd61d36457c97,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
