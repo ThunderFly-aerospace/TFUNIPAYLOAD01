ee2a1c8e0731e58be049cd3e5d67e31dc15056d0
kaklik <kaklik@users.noreply.github.com>
2026-02-13 23:01:42 +0000

    Cleanup unused files., extended build

--- SW/arduino/src/TFUNIPAYLOAD/githash.h
+++ SW/arduino/src/TFUNIPAYLOAD/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a553eed04319727b7b38695fafe22c0422774fc9,User";
+String githash = "1270af7417f61453c0e5e6064ee5eb93b283d6c0,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
