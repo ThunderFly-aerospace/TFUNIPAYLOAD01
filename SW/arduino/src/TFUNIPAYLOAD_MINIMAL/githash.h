a0dd73613d771dafcc2a1e12563155a2aa3e8708
Jakub Kákona <kaklik@mlab.cz>
2026-02-14 20:57:56 +0100

    Delete SW/arduino directory
    Sources moved to fw directory as platformIO projects.

--- SW/arduino/src/TFUNIPAYLOAD_MINIMAL/githash.h
+++
@@ -1,5 +0,0 @@
-// This file is overwritten by github actions, do not update it manually
-String githash = "76f3b367eb066ab9e3e120ff8dcaf63d72660b88,User";
-#define GHRELEASE 0
-#define GHBUILD 0
-#define GHBUILDTYPE User
