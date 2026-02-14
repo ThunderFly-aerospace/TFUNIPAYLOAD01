fe3abc16495534654dd3add4a466d97cc22e1647
roman-dvorak <roman-dvorak@users.noreply.github.com>
2026-02-14 17:36:52 +0000

    Modify workflow to adopt PlatformIO firmware CI build (#11), extended build

--- fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
+++ fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "47fdd8d64eacff53f74919e204f5b64f553e2f28,User";
-#define GHRELEASE 4
+String githash = "a38a1b8b9b3070a0ac09c98f03e20ceb8a8ea8ce,User";
+#define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
