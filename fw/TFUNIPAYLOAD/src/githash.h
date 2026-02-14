ab2f7de7444458ab1a567f86484b7ce5b2e91e16
kaklik <kaklik@users.noreply.github.com>
2026-02-14 19:42:54 +0000

    Merge pull request #12 from ThunderFly-aerospace/feature/dynamic-build-matrix, extended build

--- fw/TFUNIPAYLOAD/src/githash.h
+++ fw/TFUNIPAYLOAD/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "fe3abc16495534654dd3add4a466d97cc22e1647,User";
+String githash = "edaa79072f0f0d2a9dd626aea0eba35e53e2ac5c,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
