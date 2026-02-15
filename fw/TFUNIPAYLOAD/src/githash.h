7f6f48c53522d361bc4540c088119477f249862d
roman-dvorak <roman-dvorak@users.noreply.github.com>
2026-02-15 02:33:03 +0000

    Update workflow files, extended build

--- fw/TFUNIPAYLOAD/src/githash.h
+++ fw/TFUNIPAYLOAD/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "f48f4440ed586e9eb030824f1882661770a40c87,User";
+String githash = "dd1c253cf7b3486cfaa25919bbf3a939d1b02959,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
