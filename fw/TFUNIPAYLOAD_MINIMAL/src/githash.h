2f62602344efb963b9ee58444e0048c24d2e0122
kaklik <kaklik@users.noreply.github.com>
2026-02-16 16:43:37 +0000

    Unify PlatformIO board definition across firmware projects (#13), extended build

--- fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
+++ fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "e5f4fb8bcd0ee9526b94676111bd918068925612,User";
+String githash = "b098a8d0c989a8643c874df523720571f1975b2c,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
