c5fd7815f154a612f9616e8b05153e630fa73cf7
kaklik <kaklik@users.noreply.github.com>
2026-02-16 16:15:02 +0000

    Fix platformIO config., extended build

--- fw/TFUNIPAYLOAD/src/githash.h
+++ fw/TFUNIPAYLOAD/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "4b4633cf028c86c24c0d8e82fe03baa70dfe5a6f,User";
+String githash = "e5f4fb8bcd0ee9526b94676111bd918068925612,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
