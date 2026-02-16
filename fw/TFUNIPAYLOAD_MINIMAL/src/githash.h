55d2dc6880b18f2a250a0ef5344ce21df7f83c42
kaklik <kaklik@users.noreply.github.com>
2026-02-16 18:52:55 +0000

    Add definition of buttons and LEDs., extended build

--- fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
+++ fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "b098a8d0c989a8643c874df523720571f1975b2c,User";
+String githash = "a67a082fbecf2f9ba1474b7e570bce5497216ad5,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
