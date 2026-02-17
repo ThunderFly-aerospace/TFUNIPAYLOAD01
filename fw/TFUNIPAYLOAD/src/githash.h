5ada885a29bc17e534fc3247ffd8902651be8a10
kaklik <kaklik@users.noreply.github.com>
2026-02-17 14:05:30 +0000

    Update build_fw.yml, extended build

--- fw/TFUNIPAYLOAD/src/githash.h
+++ fw/TFUNIPAYLOAD/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "27f849d56ccf79092df70c3cdc05cd1653050165,User";
+String githash = "52ea6864f53e8355bcab750fb827dee4bbf14c2c,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
