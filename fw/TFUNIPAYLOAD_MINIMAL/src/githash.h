7c6800710cb4e840a98e2ffe454213b2edf21e76
kaklik <kaklik@users.noreply.github.com>
2026-02-17 13:48:09 +0000

    Add LED blinking., extended build

--- fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
+++ fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a67a082fbecf2f9ba1474b7e570bce5497216ad5,User";
+String githash = "27f849d56ccf79092df70c3cdc05cd1653050165,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
