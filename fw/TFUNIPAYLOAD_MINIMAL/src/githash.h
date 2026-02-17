11c488ecfb7faf11341450dc1f23add8c0239c33
kaklik <kaklik@users.noreply.github.com>
2026-02-17 20:13:07 +0000

    Configure bootloader LED., extended build

--- fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
+++ fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "70d16f699880ed5762ad73bd4ea607b21d2b55ed,User";
+String githash = "9385554eefe1010d44642d2fac2ed5ed00156ba4,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
