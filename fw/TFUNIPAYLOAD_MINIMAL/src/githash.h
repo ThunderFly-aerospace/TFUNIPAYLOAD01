e99a046455312bed695cd2c435a96a254ad3990a
kaklik <kaklik@users.noreply.github.com>
2026-03-02 21:18:11 +0000

    Update wokflow by project-unified version from AIRDOS03., extended build

--- fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
+++ fw/TFUNIPAYLOAD_MINIMAL/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "9385554eefe1010d44642d2fac2ed5ed00156ba4,User";
+String githash = "318ec3a7b8626b81ce6a27ad8e5b9c90ef731ead,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
